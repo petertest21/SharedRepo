f9c6eecb0dedad09f1a0820e85a57924647acbfc
Peter Lind <petl@kea.dk>
2022-09-20 21:36:11 +0200

    B commit 3

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         System.out.println("Another line from A");
         System.out.println("B commit 1");
         System.out.println("B commit 2");
-
+        System.out.println("B commit 3");
     }
 }
