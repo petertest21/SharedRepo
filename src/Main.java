50600910d0debca1dc2b9e6515700ef56c331757
Peter Lind <petl@kea.dk>
2022-09-20 20:23:33 +0200

    hello again (by B)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("A also says hello");
+        System.out.println("B says hello again ...");
         System.out.println("Another line from A");
     }
 }
