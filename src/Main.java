e11c183e2ae4680684be87498ad83799fa9bf8c3
petertest21 <90384249+petertest21@users.noreply.github.com>
2022-09-20 20:24:31 +0200

    add another line (by A)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("A also says hello");
+        System.out.println("Another line from A");
     }
 }
