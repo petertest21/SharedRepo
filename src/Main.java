fa6d119da94bc3fe886ade16bb76d4b4ea0b9c68
Peter Lind <petl@kea.dk>
2022-09-20 21:35:47 +0200

    B commit 1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("A also says hello");
         System.out.println("B says hello again ...");
         System.out.println("Another line from A");
+        System.out.println("B commit 1");
+
     }
 }
