554a27e4a8ea85fdc33776e6d56079371e373d24
petertest21 <90384249+petertest21@users.noreply.github.com>
2022-09-21 16:09:37 +0200

    A clears everything

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("A also says hello");
-        System.out.println("B says hello again ...");
-        System.out.println("Another line from A");
-        System.out.println("B commit 1");
-        System.out.println("B commit 2");
-        System.out.println("B commit 3");
-        System.out.println("A commit 1");
-        System.out.println("A commit 2");
+        System.out.println("A Clears Everything");      
     }
 }
