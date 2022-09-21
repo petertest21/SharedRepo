6d671cb4202275460dd999534f18a84938c6bc16
Peter Lind <petl@kea.dk>
2022-09-21 16:10:11 +0200

    B add a line

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("A Clears Everything");      
+        System.out.println("A Clears Everything");
+        System.out.println("B adds a line");
+        System.out.println("A Clears Everything");
         System.out.println("A adds a line");
         System.out.println("A adds another line");
-        
+
     }
 }
