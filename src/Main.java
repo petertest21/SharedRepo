44cafc0ccdcd3c3e21656771ef768f4eaa8c58d7
petertest21 <90384249+petertest21@users.noreply.github.com>
2022-09-21 16:11:52 +0200

    A adds another line

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("A Clears Everything");      
         System.out.println("A adds a line");
+        System.out.println("A adds another line");
+        
     }
 }
